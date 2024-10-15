3062e2ba41f3de9a132f7d7bcaafd62b09ae1e5c
Aaron Obed Girón <aogarrido.g@gmail.com>
2024-10-15 14:29:58 -0600

    Cambio: @Input() - Recibir del padre

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent {
+  public characters:Character[] =[{
+    name:'Gohan',
+    power:5000
+  },
+  {
+    name:'Pikolo',
+    power:9600
+  },
+  {
+    name:'Veguetta',
+    power:7500
+  },]
 
 }
