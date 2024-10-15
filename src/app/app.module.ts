c6db2eeaec86768bef7781b72bdf82cd23f35377
Aaron Obed Girón <aogarrido.g@gmail.com>
2024-10-15 10:21:14 -0600

    Cambio: Crear un nuevo modulo (dbz) con todos lo realcionado al componente y poder invocarlo desde el modulo principal.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 // import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
