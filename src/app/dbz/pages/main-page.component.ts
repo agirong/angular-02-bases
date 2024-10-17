738c7cc4dafa08aea0ee04af28a59d591e40b936
Aaron Obed Girón <aogarrido.g@gmail.com>
2024-10-17 12:28:55 -0600

    Servicio Privado: Para no exponer directamente el servicio en el html y concentrar la logica de negocio.

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -8,5 +8,17 @@ import { Character } from '../interfaces/character.interface';
 })
 
 export class MainPageComponent {
+
   constructor(public dbzService:DbzService){}
+  get characters():Character[]{
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id:string):void{
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter(character:Character):void{
+    this.dbzService.addCharacter(character);
+  }
 }
